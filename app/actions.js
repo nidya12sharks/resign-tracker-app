ebce74cbc381d9744809cc52b43d64add25a781a
nidya12sharks <aritonangnidia@gmail.com>
2026-08-10 10:50:18 +0700

    Delete app directory

--- app/actions.js
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { deleteCase } from '@/lib/db';
-import { revalidatePath } from 'next/cache';
-
-export async function deleteCaseAction(id) {
-  await deleteCase(id);
-  revalidatePath('/');
-}
